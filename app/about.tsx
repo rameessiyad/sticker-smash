48b8ea76ff8e8cb42ffd501bfd51e2624fae99ea
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-07-05 10:51:58 +0530

    about screen

---
+++ app/about.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    color: "#fff",
+  },
+});
