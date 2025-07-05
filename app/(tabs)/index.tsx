e3fc4503683594a0e510cf214159a32c88f519f3
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-07-05 11:45:34 +0530

    added image in home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
+import { Image } from "expo-image";
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+// import PlaceholderImage from "../../assets/images/background-image.png"
+const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>About Screen</Link>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <Link href="/about" style={styles.button}>
+        About Screen
+      </Link>
     </View>
   );
 }
@@ -24,5 +31,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: "underline",
     color: "#fff",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 328,
+    height: 448,
+    borderRadius: 18,
   },
 });
