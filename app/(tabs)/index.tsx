f2ccd90e2f07dc45c8cc3ff45fdb9d5237dfb8b7
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-07-06 15:55:11 +0530

    added image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,24 @@
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
 import { StyleSheet, View } from "react-native";
 
 const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -31,7 +45,7 @@ const styles = StyleSheet.create({
 
   imageContainer: {
     flex: 1,
-    paddingTop: 28
+    paddingTop: 28,
   },
 
   footerContainer: {
